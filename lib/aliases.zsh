089881d0522ecd34949547a59da46ea338f7b5f5
jamessthompson <jamest@runopsrun.com>
2013-02-20 19:58:15 -0600

    hack to make ls work again

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,26 +1,26 @@
 # Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
+#alias pu='pushd'
+#alias po='popd'
 
 # Basic directory operations
-alias ...='cd ../..'
-alias -- -='cd -'
+#alias ...='cd ../..'
+#alias -- -='cd -'
 
 # Super user
-alias _='sudo'
-alias please='sudo'
+#alias _='sudo'
+#alias please='sudo'
 
 #alias g='grep -in'
 
 # Show history
-alias history='fc -l 1'
+#alias history='fc -l 1'
 
 # List direcory contents
-alias lsa='ls -lah'
-alias l='ls -lA1'
-alias ll='ls -l'
-alias la='ls -lA'
-alias sl=ls # often screw this up
+#alias lsa='ls -lah'
+#alias l='ls -lA1'
+#alias ll='ls -l'
+#alias la='ls -lA'
+#alias sl=ls # often screw this up
 
-alias afind='ack-grep -il'
+#alias afind='ack-grep -il'
 
